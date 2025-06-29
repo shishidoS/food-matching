b0b895eb01b438503a6842c452d80b2153f62e9d
shishidoS <151032088+shishidoS@users.noreply.github.com>
2025-06-29 17:41:02 +0900

    DB接続保存機能実装、admin権限のみver

--- stores/admin.py
+++ stores/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient
 
-# Register your models here.
+admin.site.register(Recipe)
+admin.site.register(RecipeIngredient)
--- stores/models.py
+++ stores/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from foods.models import Ingredient  # 他アプリのモデルをインポート
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructions = models.TextField()
+    image_url = models.URLField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity_text = models.CharField(max_length=100)  # 「大さじ1」のようなテキストを保存
+
+    def __str__(self):
+        return f"{self.ingredient.name} ({self.quantity_text})"
